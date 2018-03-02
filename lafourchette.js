4aa9bcfb17d980e46fe95d137e20c9425b06d486
khangtruong <khang.truong@outlook.fr>
2018-03-02 17:55:10 +0100

    Managed to create the file to list the IDs of Lafourchette

--- lafourchette.js
+++ lafourchette.js
@@ -1,56 +1,49 @@
 const cheerio = require('cheerio');
 const request = require('request');
 const fs = require('fs');
-var michelin = require('./michelin');
+const info_mich = JSON.parse(fs.readFileSync("./list.json", "utf8"));
+var lafour_ID=[];
+var lafour_Deal=[];
 
-var michelinID=[];
-var michelinDeals=[];
-var json2= JSON.stringify(michelinDeals);
-
+for (var i = 0; i < info_mich.length; i++) {
+    getId(info_mich[i]);
+}
 
 function getId(restaurant) {
-  var url = 'https://m.lafourchette.com/api/restaurant-prediction?name=' + restaurant.name.replace(/ /g, '_').replace(/&|°/g, '').normalize('NFD').replace(/[\u0300-\u036f]/g, '');
-  request({
-        method: 'GET',
-        url: url
-      }, function(err, response, body) {
-        if (err) {
-          return reject(err);
-        }
-        var results = JSON.parse(body);
-        results.forEach(function(restaurant_found) {
-              if (restaurant.zipcode == restaurant_found.address.postal_code && restaurant.name==restaurant_found.name) {
-                  restaurant.id = restaurant_found.id;
-                  restaurant.url='https://www.lafourchette.com/restaurant/' + restaurant.name.replace(/ /g, '-').replace(/--+/g, '-') + '/' + restaurant.id;
-                  michelinID.push(restaurant);
-
+  var url = 'https://m.lafourchette.com/api/restaurant-prediction?name=' + encodeURIComponent(restaurant.name);
+  request({method: 'GET', url: url
+}, function(err, response, html) {
+        if (!err) {
+        var results = JSON.parse(html);
+        results.forEach(function(restau) {
+              if (restaurant.zipcode == restau.address.postal_code && restaurant.name==restau.name) {
+                  restaurant.id = restau.id;
+                  lafour_ID.push(restaurant);
              }
-
           })
-          var json= JSON.stringify(michelinID);
-          fs.writeFile('./fourID.json', json, 'utf8');
+          var json= JSON.stringify(lafour_ID, null, 4);
+          fs.writeFile('fourID.json', json, 'utf8');
+        }
+      })
+    }
 
+var info_fourID = JSON.parse(fs.readFileSync("./fourID.json", "utf8"));
 
-
-        })
-      }
-
+for (var i = 0; i < info_fourID.length; i++) {
+    getDeals(info_fourID[i]);
+}
 
 function getDeals(restaurant) {
   var url = 'https://m.lafourchette.com/api/restaurant/' + restaurant.id + '/sale-type';
-  request({
-        method: 'GET',
-        url: url
-      }, function(err, response, body) {
-        if (err) {
-          return reject(err);
-        }
-        var results = JSON.parse(body);
-        restaurant.deals = [];
+  request({method: 'GET', url: url
+}, function(err, response, html) {
+        if (!err) {
+        var results = JSON.parse(html);
+        restaurant.deal = [];
         results.forEach(function(deal) {
           if (deal.title != 'Simple booking') {
             if ('exclusions' in deal) {
-              restaurant.deals.push({
+              restaurant.deal.push({
                 title: deal.title,
                 exclusions: deal.exclusions ,
                 is_menu: deal.is_menu,
@@ -67,18 +60,10 @@ function getDeals(restaurant) {
           }
 
         })
-        if(restaurant.deals.length){
-           michelinDeals.push(restaurant)
-          }
-        var json2= JSON.stringify(michelinDeals);
-        fs.writeFile('.fourDeal.json', json2, 'utf8');
+        lafour_Deal.push(restaurant)
+        var json2= JSON.stringify(lafour_Deal, null, 4);
+        fs.writeFile('fourDeal.json', json2, 'utf8');
 
-      })
+      }
+    })
   }
-
-
-jsonfile.readFile('./fourID.json', function(err, resto){
-                  for(let i=0; i<112; i++){
-                     getDeals(resto[i]);
-                    }
-           });
